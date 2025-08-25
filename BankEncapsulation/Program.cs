18f62dd9d52ce09cf742309298b518f8b6e86cf9
Elias <elias.lavrich@gmail.com>
2025-08-25 10:12:05 -0400

    Create the bank class

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount myAccount = new BankAccount();
+            
+            myAccount.Deposit(987);
+            Console.WriteLine(myAccount.GetBalance());
         }
     }
 }
